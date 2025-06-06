4e5aac3195494f561727263218e75c9c9025349d
thanzia31 <thanzi2004@gmail.com>
2025-06-06 18:49:48 +0530

    Revert "Revert "Merge remote-tracking branch 'origin/Sree' into Thanzia""
    This reverts commit 617ad7f5e15cb8581906e5dcc5353c22320da021.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {provideEchartsCore } from 'ngx-echarts';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideEchartsCore } from 'ngx-echarts'
