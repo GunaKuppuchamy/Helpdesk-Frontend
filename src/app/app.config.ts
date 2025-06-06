d430189cd602f582b957617b12d3896c3af2b05d
thanzia31 <thanzi2004@gmail.com>
2025-06-06 16:48:16 +0530

    Integration change 7

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideEchartsCore } from 'ngx-echarts'
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(),
